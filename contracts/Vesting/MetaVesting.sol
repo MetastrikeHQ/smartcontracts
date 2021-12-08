e0231210eae96c8b8081c6882d78ed2df6e3ba9e
pavunato <15843773+pavunato@users.noreply.github.com>
2021-12-08 21:19:38 +0700

    Vesting Token

---
+++ contracts/Vesting/MetaVesting.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract MetaVesting is Ownable {
+    using SafeERC20 for IERC20;
+    IERC20 public mtsERC20;
+
+    struct VestingStrategy {
+        uint256 tge;
+        uint256 cliff;
+        uint256 linearDuration;
+    }
+
+    struct VestingInfo {
+        uint256 amount;
+        uint256 claimed;
+        uint256 lastClaim;
+    }
+
+    uint256 public tgeTime;
+
+    mapping (uint256 => VestingStrategy) public vestingStrategy;
+    mapping (address => mapping (uint256 => VestingInfo)) public userToVesting;
+    mapping (address => uint256[]) public userVesting;
+
+    constructor (address _mtsERC20, uint256 _tgeTime) {
+        mtsERC20 = IERC20(_mtsERC20);
+        tgeTime = _tgeTime;
+    }
+
+    function setupVestingStrategy(uint256 _id, uint256 _tgePercent, uint256 _cliffSecs, uint256 _linearSecs) external onlyOwner {
+        vestingStrategy[_id] = VestingStrategy(_tgePercent, _cliffSecs, _linearSecs);
+    }
+
+    function setupVestingUser(uint256 _strategyId, uint256 _amount, address[] calldata _users) external onlyOwner{
+        for(uint256 i =0; i < _users.length; i++ ) {
+            userToVesting[_users[i]][_strategyId].amount += _amount;
+            userToVesting[_users[i]][_strategyId].claimed = 0;
+            userToVesting[_users[i]][_strategyId].lastClaim = tgeTime;
+
+        }
+    }
+
+    function claimable(address _user, uint256 _strategyId) public view returns (uint256) {
+        VestingInfo memory userInfo = userToVesting[_user][_strategyId];
+        VestingStrategy memory vestingInfo = vestingStrategy[_strategyId];
+        console.log(vestingInfo.tge, userInfo.amount);
+        uint256 _claimTge = vestingInfo.tge * userInfo.amount / 1000;
+        uint256 _amountAfterTge = userInfo.amount - _claimTge;
+        uint256 _timeSpent;
+        uint256 _claiming;
+        console.log(block.timestamp);
+        console.log(vestingInfo.cliff);
+        console.log(tgeTime);
+        console.log(_claimTge, _amountAfterTge, _timeSpent, _claiming);
+        if (userInfo.claimed < _claimTge) {
+            _claiming = _claimTge;
+        }
+        if (tgeTime + vestingInfo.cliff < block.timestamp) {
+            if (tgeTime + vestingInfo.cliff < userInfo.lastClaim) {
+                _timeSpent = block.timestamp - (tgeTime + vestingInfo.cliff);
+            } else {
+                _timeSpent = block.timestamp - userInfo.lastClaim;
+            }
+        }
+        // console.log("TimeSpent: ", _timeSpent);
+        _claiming += _timeSpent * _amountAfterTge / vestingInfo.linearDuration;
+        if (_claiming > userInfo.amount - userInfo.claimed) {
+            _claiming = userInfo.amount - userInfo.claimed;
+        }
+        return _claiming;
+    }
+
+    function claim(uint256 _strategyId) public {
+        VestingInfo storage userInfo = userToVesting[msg.sender][_strategyId];
+        VestingStrategy storage vestingInfo = vestingStrategy[_strategyId];
+        require(userInfo.amount > 0, "MetaVesting: You don't have allocation for this type!");
+        require(userInfo.claimed < userInfo.amount, "MetaVesting: You already received fully your allocation!");
+        uint256 claiming;
+        uint256 claimTge = vestingInfo.tge * userInfo.amount / 1000;
+        uint256 amountAfterTge = userInfo.amount - claimTge;
+        uint256 timeSpent;
+        if (userInfo.claimed < claimTge) {
+            claiming = claimTge;
+        }
+
+        if (tgeTime + vestingInfo.cliff < block.timestamp) {
+            if (tgeTime + vestingInfo.cliff < userInfo.lastClaim) {
+                timeSpent = block.timestamp - (tgeTime + vestingInfo.cliff);
+            } else {
+                timeSpent = block.timestamp - userInfo.lastClaim;
+            }
+        }
+
+        claiming += timeSpent * amountAfterTge / vestingInfo.linearDuration;
+
+        if (claiming > userInfo.amount - userInfo.claimed) {
+            claiming = userInfo.amount - userInfo.claimed;
+        }
+
+        userInfo.claimed += claiming;
+        userInfo.lastClaim = block.timestamp;
+
+        require(claiming > 0, "MetaVesting: We already vested all tokens!");
+        mtsERC20.safeTransfer(msg.sender, claiming);
+    }
+
+    function withdraw(address _to) external onlyOwner {
+        mtsERC20.safeTransfer(_to, mtsERC20.balanceOf(address(this)));
+    }
+
+}
---
+++ test/MetaVesting.test.js
@@ -0,0 +1,74 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+const hre = require('hardhat')
+
+describe("Token contract", function () {
+  // Mocha has four functions that let you hook into the the test runner's
+  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
+
+  // They're very useful to setup the environment for tests, and to clean it
+  // up after they run.
+
+  // A common pattern is to declare some variables, and assign them in the
+  // `before` and `beforeEach` callbacks.
+
+  let MtsTokenContract;
+  let MetaVestingContract;
+
+  let mtsToken;
+  let metaVesting;
+
+  let owner;
+  let user1;
+  let user2;
+  let user3;
+  let addrs;
+
+  let now;
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+
+    // Get the ContractFactory and Signers here.
+    //MetaVestingContract
+    MetaVestingContract = await ethers.getContractFactory("MetaVesting");
+    //MetaStrike Token
+    MtsTokenContract = await ethers.getContractFactory("MetaStrike");
+    [owner, user1, user2, user3, user4, user5, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens onces its transaction has been
+    // mined.
+    mtsToken = await MtsTokenContract.deploy();
+    now = Math.round(new Date().getTime() / 1000);
+
+    metaVesting = await MetaVestingContract.deploy(mtsToken.address, now);
+  });
+
+  // You can nest describe calls to create subsections.
+  describe("Deployment", function () {
+
+    it("tge 15%, cliff 200, linear sec", async function () {
+        // function setupVestingStrategy(uint256 _id, uint256 _tgePercent, uint256 _cliffSecs, uint256 _linearSecs)
+        await metaVesting.setupVestingStrategy(0, 150, 200, 2000);
+        await metaVesting.setupVestingUser(0, 100000, [user1.address]);
+        await mtsToken.transfer(metaVesting.address, 1000000);
+        let user1Info = await metaVesting.userToVesting(user1.address, 0);
+        console.log(user1Info.amount.toString(), user1Info.claimed.toString(), user1Info.lastClaim.toString());
+        let user1Claimable = await metaVesting.claimable(user1.address, 0);
+        console.log('User 1 Claimable: ', user1Claimable.toString());
+        await network.provider.send("evm_increaseTime", [4000])
+        await network.provider.send("evm_mine")
+        user1Claimable = await metaVesting.claimable(user1.address, 0);
+        console.log('User 1 Claimable: ', user1Claimable.toString());
+        await metaVesting.connect(user1).claim(0);
+        let user1Bl = await mtsToken.balanceOf(user1.address);
+        console.log("User 1 Balance: ", user1Bl.toString());
+        user1Claimable = await metaVesting.claimable(user1.address, 0);
+        console.log('User 1 Claimable: ', user1Claimable.toString());
+        await metaVesting.connect(user1).claim(0);
+    });
+  });
+});
