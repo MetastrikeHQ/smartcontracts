d5e10a1c05a43fd66bf10d451846873890ed94a1
pavunato <15843773+pavunato@users.noreply.github.com>
2022-01-07 21:34:29 +0700

    submit setup multiple user

---
+++ scripts/4.SetupVU.js
@@ -0,0 +1,79 @@
+const hre = require('hardhat')
+
+const getAllRows = async (fileName) => {
+	return new Promise((resolve, reject) => {
+		const csv = require('csv-parser')
+		const fs = require('fs')
+		let results = []
+
+		fs.createReadStream(fileName)
+			.pipe(csv())
+			.on('data', (data) => results.push(data))
+			.on('end', () => {
+				resolve(results)
+			})
+	})
+}
+
+async function main () {
+
+	const addressesMainnet = {
+		pancakeswap_router: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
+        vestingApp = ''
+    }
+
+	const addressesTestnet = {
+		pancakeswap_router: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
+        vestingApp = ''
+	}
+
+
+	const accounts = await hre.ethers.getSigners()
+	const MetaStrikeTokenContract = await hre.ethers.getContractFactory('MetaStrike')
+	// const MetaStakingContract = await hre.ethers.getContractFactory('MetaStaking')
+
+	const options = {
+		gasLimit: 4000000
+	}
+
+	const mtsToken = await MetaStrikeTokenContract.deploy()
+
+    const MetaVestingContract = await ethers.getContractFactory("MetaVesting");
+    const now = Math.round(new Date().getTime() / 1000);
+    const metaVesting = await MetaVestingContract.attach()
+    
+    let addressesFile = './scripts/CSV/address_list.csv'
+	let resAllRows = await getAllRows(addressesFile)
+	let tx, res
+	let totalRow = 0
+	let eachTx = 400
+	for (let i = 0; i < resAllRows.length; i += eachTx) {
+		let addresses = []
+		let amounts = []
+		for (let j = 0; j < eachTx; j++) {
+			try {
+				const row = resAllRows[i + j]
+				for (const key of Object.keys(row)) {
+					row[key.trim()] = row[key]
+				}
+				addresses.push(row.address)
+			} catch {
+				break
+			}
+		}
+		console.log(addresses)
+        await metaVesting.setupVestingUser(0, 100000, addresses);
+		res = await tx.wait()
+		console.log('Batch ', i / eachTx, res.status, 'with ', res.transactionHash)
+    }
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error)
+		process.exit(1)
+	})
