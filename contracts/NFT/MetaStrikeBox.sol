43ccd8b1aa56a4e1a694ca5f998c2d16dc58ca4c
pavunato <15843773+pavunato@users.noreply.github.com>
2022-04-03 21:35:05 +0700

    random color

--- contracts/NFT/MetaStrikeBox.sol
+++ contracts/NFT/MetaStrikeBox.sol
@@ -1,48 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 interface IMetaStrikeCore {
-    function safeMint(address to, uint256 _weapon, uint256 _skin, uint8 _tier,  uint8 _slot) external;
+    function safeMint(address to, uint256 _weapon, uint256 _skin, uint8 _color, uint8 _tier,  uint8 _slot) external;
 }
 
 /// @custom:security-contact security@metastrike.io
-contract MetaStrikeBox is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
-    using Counters for Counters.Counter;
+contract MetaStrikeBox is ERC1155, Pausable, AccessControl, ERC1155Burnable {
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    Counters.Counter private _tokenIdCounter;
 
     address public metastrikeCore;
+    string public constant name = "";
 
     struct BoxInfo {
         uint256 weapons;
         uint8 tier;
         uint256 skins;
+        uint8 colors;
         uint8[] slots;
         uint256[] weightedSlots;
     }
 
-    mapping (uint8 => BoxInfo) public boxesInfo;
+    mapping (uint256 => BoxInfo) public boxesInfo;
     mapping (uint8 => uint256) public boxTypeToTimeLock;
-    mapping (uint256 => uint8) public idToBoxType;
 
-    constructor(address _metastrikeCore) ERC721("MetaStrikeBox", "MTS_BOX") {
+    constructor(address _metastrikeCore) ERC1155("https://resource.metastrike.io/box/{id}.json") {
         metastrikeCore = _metastrikeCore;
         grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         grantRole(PAUSER_ROLE, msg.sender);
         grantRole(MINTER_ROLE, msg.sender);
-    }
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://resource.metastrike.io/box/{id}.json";
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -53,36 +46,36 @@ contract MetaStrikeBox is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC
         _unpause();
     }
 
-    function ownedBy(address _user) external view returns (uint256[] memory nfts) {
-        uint256 owning = ERC721.balanceOf(_user);
-        nfts = new uint256[] (owning);
-        for (uint256 i = 0; i < owning; i ++ ) { 
-            nfts[i] = tokenOfOwnerByIndex(_user,i);
-        }
-    }
-
-    function setupBox(uint8 _boxId, uint256 _weapons, uint8 _tier, uint256 _skin, uint8[] calldata _slots, uint256[] calldata _weightedSlots) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setupBox(uint8 _boxId, uint256 _weapons, uint8 _tier, uint256 _skin, uint8 _color, uint8[] calldata _slots, uint256[] calldata _weightedSlots) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(boxesInfo[_boxId].weapons == 0, "Box already set up!");
-        boxesInfo[_boxId] = BoxInfo(_weapons, _tier, _skin, _slots, _weightedSlots);
+        boxesInfo[_boxId] = BoxInfo(_weapons, _tier, _skin, _color, _slots, _weightedSlots);
+    }
+    
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        require(boxesInfo[id].weapons != 0, "Box not set up!");
+        _mint(account, id, amount, data);
     }
 
-    function safeMint(address to, uint8 _boxId) public onlyRole(MINTER_ROLE) {
-        require(boxesInfo[_boxId].weapons != 0, "Box not set up!");
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        idToBoxType[tokenId] = _boxId;
-        _safeMint(to, tokenId);
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mintBatch(to, ids, amounts, data);
     }
 
     function openBox(uint256 _id) external {
         require(msg.sender == tx.origin, "Nope lah!");
-        burn(_id);
-        BoxInfo memory boxInfo = boxesInfo[idToBoxType[_id]];
+        burn(msg.sender, _id, 1);
+        BoxInfo memory boxInfo = boxesInfo[_id];
         uint256 weaponType = _randomUint256(boxInfo.weapons);
         uint256 weaponSkin = _randomUint256(boxInfo.skins);
+        uint8 weaponColor = uint8(_randomUint256(boxInfo.colors));
         uint8 slotsDraw = boxInfo.slots[_weightedRandomArray(boxInfo.weightedSlots)];
         if (slotsDraw != 0) {
-            IMetaStrikeCore(metastrikeCore).safeMint(msg.sender, weaponType, weaponSkin, boxInfo.tier, slotsDraw-1);
+            IMetaStrikeCore(metastrikeCore).safeMint(msg.sender, weaponType, weaponSkin, weaponColor, boxInfo.tier, slotsDraw-1);
         }
     }
 
@@ -108,12 +101,12 @@ contract MetaStrikeBox is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC
         rnd = rnd % ranged;
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
         whenNotPaused
-        override(ERC721, ERC721Enumerable)
+        override
     {
-        super._beforeTokenTransfer(from, to, tokenId);
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
     // The following functions are overrides required by Solidity.
@@ -121,7 +114,7 @@ contract MetaStrikeBox is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, AccessControl)
+        override(ERC1155, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
